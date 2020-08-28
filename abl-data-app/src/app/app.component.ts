3a05843e1bfd054ce3ef322fd4bec72b8a634dae
gwbcho <gregwbcho@gmail.com>
2020-08-28 13:10:59 -0700

    ABL-000 Added graphing components.

--- abl-data-app/src/app/app.component.ts
+++ abl-data-app/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'abl-data-app';
+export class AppComponent implements OnInit {
+    title = 'abl-data-app';
+    chart: Chart;
+
+    ngOnInit() {
+        // retrieve chart canvas ID
+        const ctx = document.getElementById('graphCanvas');
+        // instantiate Chart object
+        this.chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                datasets: [
+                    {
+                        label: 'Data From All (A, B, C) Sources',
+                        data: [],
+                        backgroundColor: 'black',
+                        borderColor: 'black',
+                        fill: false
+                    },
+                    {
+                        label: 'Data From Source A',
+                        data: [],
+                        backgroundColor: 'red',
+                        borderColor: 'red',
+                        fill: false
+                    },
+                    {
+                        label: 'Data From Source B',
+                        data: [],
+                        backgroundColor: 'blue',
+                        borderColor: 'blue',
+                        fill: false
+                    },
+                    {
+                        label: 'Data From Source C',
+                        data: [],
+                        backgroundColor: 'green',
+                        borderColor: 'green',
+                        fill: false
+                    }
+                ]
+            }
+        });
+    }
+
 }
